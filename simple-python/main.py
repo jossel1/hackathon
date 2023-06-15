f522d9e83bd13c5c354cbd0308eb5c8f49ae0a51
josselin.tobelem <josselin.tobelem@adevinta.com>
2023-06-15 15:36:06 +0200

    dev

---
+++ simple-python/main.py
@@ -0,0 +1,24 @@
+def extract_weather_description(weather_data):
+    weather_descriptions = []
+    for weather in weather_data["weather"]:
+        description = weather["description"]
+        score = calculate_weather_score(description)
+        if score >= 5:
+            weather_descriptions.append(description)
+    return weather_descriptions
+
+
+def calculate_weather_score(description):
+    weather_scores = {
+        "clear sky": 10,
+        "few clouds": 8,
+        "scattered clouds": 6,
+        "broken clouds": 4,
+        "overcast clouds": 2,
+        "light rain": 5,
+        "moderate rain": 3,
+        "heavy rain": 1,
+    }
+
+    score = weather_scores.get(description, 0)
+    return score
